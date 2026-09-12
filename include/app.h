43f558ef305802f3edb1c35ddf58d0bbcb94ebb7
hmwc0006-maker <hmwc0006@gmail.com>
2026-09-13 02:31:46 +0800

    Add include/app.h

---
+++ include/app.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "include/cef_app.h"
+
+class App : public CefApp, public CefCommandLineHandler {
+public:
+    App();
+    ~App() override;
+    CefRefPtr<CefCommandLineHandler> GetCommandLineHandler() override { return this; }
+    void OnBeforeCommandLineProcessing(const CefString&, CefRefPtr<CefCommandLine>) override;
+    IMPLEMENT_REFCOUNTING(App);
+    DISALLOW_COPY_AND_ASSIGN(App);
+};
