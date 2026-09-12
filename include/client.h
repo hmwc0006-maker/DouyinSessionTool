0ccd561fd98ae011f534ebed13f85aed07e8e4ea
hmwc0006-maker <hmwc0006@gmail.com>
2026-09-13 02:31:48 +0800

    Add include/client.h

---
+++ include/client.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "include/cef_client.h"
+#include "include/cef_life_span_handler.h"
+#include "include/cef_load_handler.h"
+#include <string>
+
+class Client : public CefClient, public CefLifeSpanHandler, public CefLoadHandler {
+public:
+    Client();
+    ~Client() override;
+    CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() override;
+    CefRefPtr<CefLoadHandler> GetLoadHandler() override;
+    void OnAfterCreated(CefRefPtr<CefBrowser>) override;
+    void OnBeforeClose(CefRefPtr<CefBrowser>) override;
+    void OnLoadEnd(CefRefPtr<CefBrowser>, CefRefPtr<CefFrame>, int) override;
+private:
+    CefRefPtr<CefBrowser> browser_;
+    IMPLEMENT_REFCOUNTING(Client);
+    DISALLOW_COPY_AND_ASSIGN(Client);
+};
