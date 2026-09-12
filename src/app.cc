0bb55f4cee5bfa4cfcbd953dc9ca1581e53f7ed4
hmwc0006-maker <hmwc0006@gmail.com>
2026-09-13 02:31:50 +0800

    Add src/app.cc

---
+++ src/app.cc
@@ -0,0 +1,12 @@
+﻿#include "app.h"
+
+App::App() {}
+App::~App() {}
+
+void App::OnBeforeCommandLineProcessing(
+    const CefString& process_type,
+    CefRefPtr<CefCommandLine> command_line) {
+    command_line->AppendSwitch("disable-gpu-compositing");
+    command_line->AppendSwitch("disable-gpu");
+    command_line->AppendSwitch("no-proxy-server");
+}
